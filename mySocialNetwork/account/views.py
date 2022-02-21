9246e3e922f4e8bbb1ab669506cbb48dfcdbb8df
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-21 05:09:07 -0500

    write user registration view code and add messages.html in includes in base templates

--- mySocialNetwork/account/views.py
+++ mySocialNetwork/account/views.py
@@ -1,13 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from .forms import UserRegistrationForm
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 # Create your views here.
 
 class RegisterView(View):
+    form_class = UserRegistrationForm
+    template_name = 'account/register.html'
+
     def get(self, request):
-        form = UserRegistrationForm()
-        return render(request, 'account/register.html', {'form':form})
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
 
     def post(self, request):
-        pass
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form_cleaned_data = form.cleaned_data
+            User.objects.create_user(form_cleaned_data['username'], form_cleaned_data['email'], form_cleaned_data['password'])
+            messages.success(request, 'You registered successfully', 'success')
+            return redirect('home:home')
+        return render(request, self.template_name, {'form':form})

df790fe497a7bcb4c01fec10c8a97c123beee4cb
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-21 04:21:59 -0500

    change form fields with widgets

--- mySocialNetwork/account/forms.py
+++ mySocialNetwork/account/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Your username'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Your email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Your password'}))

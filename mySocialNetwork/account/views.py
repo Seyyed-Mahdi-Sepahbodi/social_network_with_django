4b7a2857d5c22df1be9bec064543f2442be056f0
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-21 06:07:58 -0500

    write user login view and user login form and add path in urls

--- mySocialNetwork/account/views.py
+++ mySocialNetwork/account/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from .forms import UserRegistrationForm, UserLoginForm
 from django.contrib.auth.models import User
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 
-class RegisterView(View):
+class UserRegisterView(View):
     form_class = UserRegistrationForm
     template_name = 'account/register.html'
 
@@ -21,4 +22,25 @@ class RegisterView(View):
             User.objects.create_user(form_cleaned_data['username'], form_cleaned_data['email'], form_cleaned_data['password1'])
             messages.success(request, 'You registered successfully', 'success')
             return redirect('home:home')
+        return render(request, self.template_name, {'form':form})
+
+
+class UserLoginView(View):
+    form_class = UserLoginForm
+    template_name = 'account/login.html'
+
+    def get(self, request):
+        form = self.form_class()
+        return render(request, self.template_name, {'form':form})
+    
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            form_cleaned_data = form.cleaned_data
+            user = authenticate(request, username=form_cleaned_data['username'], password=form_cleaned_data['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'You logged in successfully.', 'success')
+                return redirect('home:home')
+            messages.error(request, 'username or password is wrong!', 'warning')
         return render(request, self.template_name, {'form':form})

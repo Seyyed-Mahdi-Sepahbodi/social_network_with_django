d19dbce7c46f706f52fcbb306a1390acd40b4dda
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-22 08:32:35 -0500

    customizing admin panel for posts

--- mySocialNetwork/home/admin.py
+++ mySocialNetwork/home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug', 'updated_at')
+    search_fields = ('slug',)
+    list_filter = ('updated_at',)
+    raw_id_fields = ('user',)
+
--- mySocialNetwork/home/models.py
+++ mySocialNetwork/home/models.py
@@ -10,4 +10,7 @@ class Post(models.Model):
     slug = models.SlugField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.slug
  

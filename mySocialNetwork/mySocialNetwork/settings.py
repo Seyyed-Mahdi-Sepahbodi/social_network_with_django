9b58b89689c6f550444e8edf5f312f0e0bdbbe9c
Seyyed-Mahdi-Sepahbodi <seyyedmahdisepahbodi@gmail.com>
2022-02-23 07:46:45 -0500

    add password reset views and templates and add gitignore

--- mySocialNetwork/mySocialNetwork/settings.py
+++ mySocialNetwork/mySocialNetwork/settings.py
@@ -132,3 +132,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = '/account/login/'
+
+
+# Gmail Config
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'seyyedmahdisepahbodi@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'tgvftolssgxuuvub'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Fact Media'
